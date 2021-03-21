d09658a32ac9f1b634f5b7e6b727fd089ebb25b1
imckain <ianmmckain@gmail.com>
2021-03-20 22:13:59 -0400

    set up gatsby build for EPI; homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
-import React from "react"
+import React from "react";
+import { Link } from 'gatsby';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <Link to="/contact">Contact Me</Link>
+      <Link to="/">Home</Link>
+    </div>
+  )
 }
