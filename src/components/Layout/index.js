e015d11d6c43bc88aecb97211dabbe241fb943cd
imckain <ianmmckain@gmail.com>
2021-03-21 00:01:22 -0400

    module sass styling

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,12 +3,15 @@ import React from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
 
+import '../../styles/reset.scss';
+import * as styles from './Layout.module.scss';
+
 const Layout = (props) => (
-    <div>
+    <div className={styles.Layout}>
         <Header />
-            <main>
-                { props.children }
-            </main>
+        <main>
+            { props.children }
+        </main>
         <Footer />
     </div>
 );
