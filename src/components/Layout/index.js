501138ba1787915a9fb702488da75ce27cb40a12
imckain <ianmmckain@gmail.com>
2021-03-23 23:24:05 -0400

    pulled data from graphQL query

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Helmet from 'react-helmet';
 
 import Header from '../Header';
 import Footer from '../Footer';
@@ -6,14 +8,33 @@ import Footer from '../Footer';
 import '../../styles/reset.scss';
 import * as styles from './Layout.module.scss';
 
-const Layout = (props) => (
-    <div className={styles.Layout}>
-        <Header />
-        <main>
-            { props.children }
-        </main>
-        <Footer />
-    </div>
-);
+const Layout = (props) => {
+    const data = useStaticQuery(graphql`
+        query MyQuery {
+            site {
+                siteMetadata {
+                    siteTitle
+                }
+            }
+        }
+    `);
+
+    const { siteTitle } = data.site.siteMetadata;
+    
+    return(
+        <Fragment>
+            <Helmet>
+                <title>{siteTitle}</title>
+            </Helmet>
+            <div className={styles.Layout}>
+                <Header siteTitle={siteTitle} />
+                <main>
+                    { props.children }
+                </main>
+                <Footer siteTitle={siteTitle} />
+            </div>
+        </Fragment>
+    );
+}
 
 export default Layout;
