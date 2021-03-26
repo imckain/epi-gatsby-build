210f0a26c7924acf3b1fc33261879bf34ccf76ac
imckain <ianmmckain@gmail.com>
2021-03-26 00:01:08 -0400

    created slug paths for links

---
+++ gatsby-node.js
@@ -0,0 +1,45 @@
+const { createFilePath } = require(`gatsby-source-filesystem`);
+const path = require(`path`);
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+    if(node.internal.type === 'MarkdownRemark') {
+        const { createNodeField } = actions;
+        const slug = createFilePath({ 
+            node,
+            getNode,
+        });
+        createNodeField({
+            node,
+            name: `slug`,
+            value: slug,
+        });
+    };
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions;
+
+    const result = await graphql(`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `);
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+            path: node.fields.slug,
+            component: path.resolve(`./src/templates/project.js`),
+            context: {
+                slug: node.fields.slug
+            },
+        });
+    });
+};
