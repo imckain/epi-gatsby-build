497cbd4eb85ff851c42a3324a733ec9ac32207f7
imckain <ianmmckain@gmail.com>
2021-03-28 23:26:58 -0400

    adding banner for and content for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-import SplashScreen from '../components/SplashScreen'
+import SplashScreen from '../components/SplashScreen';
+import HomeContent from '../components/HomeContent';
 
 export default function Home() {
   return (
     <div>
       <SplashScreen />
+      <div className={styles.SplashBanner}></div>
+      <HomeContent />
     </div>
   )
 }
