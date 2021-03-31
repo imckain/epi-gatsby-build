b941a926892fa71c4df676760849f97ec782f8dc
imckain <ianmmckain@gmail.com>
2021-03-30 22:13:21 -0400

    contact card component for home page

---
+++ src/components/ContactCard/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import * as styles from './ContactCard.module.scss';
+
+const ContactCard = (props) => {
+
+    return(
+        <div>
+            <div className={styles.Logo}>
+
+            </div>
+        </div>
+    );
+};
+
+export default ContactCard;
