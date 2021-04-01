cfb3d905cbee75cbf19ae83baae14d95eb778f5d
imckain <ianmmckain@gmail.com>
2021-03-31 21:58:00 -0400

    added wrapper containers

--- src/components/SplashScreen/index.js
+++ src/components/SplashScreen/index.js
@@ -28,16 +28,16 @@ const SplashScreen = (props) => {
             <div className={styles.ShowcaseContainer}>
                 <div className={styles.ShowcaseList}>
                     <ul>
-                        <li>
+                        <li className={styles.Li1}>
                             <p className={styles.P1}>Reliable, Quality Engineering</p>
                         </li>
-                        <li>
+                        <li className={styles.Li2}>
                             <p className={styles.P2}>Advanced Techniques</p>
                         </li>
-                        <li>
+                        <li className={styles.Li3}>
                             <p className={styles.P3}>Modern Technologies</p>
                         </li>
-                        <li>
+                        <li className={styles.Li4}>
                             <p className={styles.P4}>Proven Record &nbsp;</p>
                         </li>
                     </ul>
