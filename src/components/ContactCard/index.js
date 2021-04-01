5fa3146ae83ba8cef55daf8d1379a66ef50cc88c
imckain <ianmmckain@gmail.com>
2021-03-31 21:50:08 -0400

    add content

--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -5,9 +5,27 @@ import * as styles from './ContactCard.module.scss';
 const ContactCard = (props) => {
 
     return(
-        <div>
-            <div className={styles.Logo}>
-
+        <div className={styles.ContactCard}>
+            <div className={styles.ContactCardContent}>
+                <div className={styles.Logo}>
+                    EPI
+                    <span>EST. 1993</span>
+                </div>
+                <span>Contact us:</span>
+                <div className={styles.ContactOptions}>
+                    <div className={styles.ContactItem}>
+                        <i></i>
+                        <a href="tel:+">(304) 555-5555</a>
+                    </div>
+                    <div className={styles.ContactItem}>
+                        <i></i>
+                        <a href="tel:+">(304) 555-5555</a>
+                    </div>
+                    <div className={styles.ContactItem}>
+                        <i></i>
+                        <a href="mailto:">epi@mail.com</a>
+                    </div>
+                </div>
             </div>
         </div>
     );
