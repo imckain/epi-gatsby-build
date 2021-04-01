47178f5b97753fbc95325a35a65561e85a80f84e
imckain <ianmmckain@gmail.com>
2021-03-31 23:27:48 -0400

    add icons

--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -12,18 +12,19 @@ const ContactCard = (props) => {
                     <span>EST. 1993</span>
                 </div>
                 <span>Contact us:</span>
+                <div className={styles.ContactCardRule}></div>
                 <div className={styles.ContactOptions}>
                     <div className={styles.ContactItem}>
-                        <i></i>
-                        <a href="tel:+">(304) 555-5555</a>
+                        <span className="material-icons">phone</span>
+                        <a className={styles.Phone} href="tel:+">304.555.5555</a>
                     </div>
                     <div className={styles.ContactItem}>
-                        <i></i>
-                        <a href="tel:+">(304) 555-5555</a>
+                        <span className="material-icons">phone</span>
+                        <a className={styles.Phone} href="tel:+">304.555.5555</a>
                     </div>
                     <div className={styles.ContactItem}>
-                        <i></i>
-                        <a href="mailto:">epi@mail.com</a>
+                        <span className="material-icons">email</span>
+                        <a className={styles.Email} href="mailto:">epi@mail.com</a>
                     </div>
                 </div>
             </div>
